ba4d29fab000f0ec18319893354e3aea1ec3e696
BrienB <barrdom@hotmail.com>
2021-01-30 03:05:37 -0500

    added get /api/notes route to return notes from db.json

--- server.js
+++ server.js
@@ -8,12 +8,34 @@ var app = express();
 
 var PORT = process.env.PORT || 8080;
 
+var readFileAsync = util.promisify(fs.readFile);
+
+function getNotes(){
+
+    return readFileAsync(__dirname + "/db.json", "utf8");
+
+};
+
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.get("/notes", function(req, res) {
     res.sendFile(path.join(__dirname, "notes.html"));
+});
+
+app.get("/api/notes", async function(req, res) {
+    // var data = fs.readFile(__dirname + "/db.json", err => err ? console.error(err) : console.log("Success!"));
+    var notes = ["Hot dog"];
+    await getNotes()
+    .then(function(res){
+        notes = JSON.parse(res);
+        console.log(notes);
+        return notes;
+    })
+    .catch(err => err ? console.error(err) : console.log("Post Successful!"));
+    console.log(notes);
+    res.json(notes);
 });
 
 app.get("*", function(req, res) {
