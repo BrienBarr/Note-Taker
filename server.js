68390fa450ee9147a9470eea03202d815ab16108
BrienB <barrdom@hotmail.com>
2021-01-30 02:19:25 -0500

    added get /notes route

--- server.js
+++ server.js
@@ -12,6 +12,9 @@ var PORT = process.env.PORT || 8080;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+app.get("/notes", function(req, res) {
+    res.sendFile(path.join(__dirname, "notes.html"));
+});
 
 app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
