665cf9bfdb96fd7163a88ac9bd31413a26b2a97f
BrienB <barrdom@hotmail.com>
2021-01-30 21:56:47 -0500

    removed console logs and commented out code

--- server.js
+++ server.js
@@ -25,16 +25,13 @@ app.get("/notes", function(req, res) {
 });
 
 app.get("/api/notes", async function(req, res) {
-    // var data = fs.readFile(__dirname + "/db.json", err => err ? console.error(err) : console.log("Success!"));
     var notes = [];
     await getNotes()
     .then(function(res){
         notes = JSON.parse(res);
-        console.log(notes);
         return notes;
     })
     .catch(err => err ? console.error(err) : console.log("Post Successful!"));
-    console.log(notes);
     res.json(notes);
 });
 
@@ -44,16 +41,10 @@ app.get("*", function(req, res) {
 
 app.post("/api/notes", async function(req, res) {
     var newNote = req.body;
-    console.log(newNote);
     await getNotes()
     .then(function(res){
-        console.log(res);
-        // res.json(newNote);
         notes = JSON.parse(res);
-        console.log(notes);
-        // console.log(typeof notes + ": " + notes);
         notes.push(newNote);
-        console.log(typeof notes);
         data = JSON.stringify(notes);
         fs.writeFile(__dirname + "/db/db.json", data, err => err ? console.error(err) : console.log("New notes written successfully!"));
     })
